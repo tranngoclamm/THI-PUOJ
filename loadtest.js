b39dc93a032fcff09244a81cb5b75e35d69e7d4d
your name <21010291@st.phenikaa-uni.edu.vn>
2025-04-15 13:32:33 +0700

    Convert to Django

--- loadtest.js
+++ loadtest.js
@@ -5,8 +5,8 @@ const body = fs.readFileSync('post.json', 'utf-8');
 
 autocannon({
   url: 'http://127.0.0.1:8000/run_code/',
-  connections: 100,
-  duration: 40,
+  connections: 15,
+  duration: 10,
   timeout: 15000  ,
   method: 'POST',
   headers: {
